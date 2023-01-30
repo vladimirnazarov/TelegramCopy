be25c0df3a4d33541be69a3a0ab5e266bd99bc6d
Vladimir Nazarov <tooldiamond2014@gmail.com>
2023-01-30 21:54:28 +0300

    User register by phone number 1

--- app/src/main/java/com/vnazarov/telegramclone/ui/fragments/RegistrationCodeFragment.kt
+++ app/src/main/java/com/vnazarov/telegramclone/ui/fragments/RegistrationCodeFragment.kt
@@ -7,7 +7,7 @@ import com.vnazarov.telegramclone.utilits.AppTextWatcher
 import com.vnazarov.telegramclone.utilits.showToast
 import kotlinx.android.synthetic.main.fragment_registration_code.*
 
-class RegistrationCodeFragment : Fragment(R.layout.fragment_registration_code) {
+class RegistrationCodeFragment(mPhoneNumber: String, id: String) : Fragment(R.layout.fragment_registration_code) {
 
     override fun onStart() {
         super.onStart()
--- app/src/main/java/com/vnazarov/telegramclone/ui/fragments/RegistrationPhoneFragment.kt
+++ app/src/main/java/com/vnazarov/telegramclone/ui/fragments/RegistrationPhoneFragment.kt
@@ -1,16 +1,51 @@
 package com.vnazarov.telegramclone.ui.fragments
 
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import com.vnazarov.telegramclone.MainActivity
 import com.vnazarov.telegramclone.R
+import com.vnazarov.telegramclone.activities.RegisterActivity
+import com.vnazarov.telegramclone.utilits.replaceActivity
 import com.vnazarov.telegramclone.utilits.replaceFragment
 import com.vnazarov.telegramclone.utilits.showToast
 import kotlinx.android.synthetic.main.fragment_registration_phone.*
+import java.util.concurrent.TimeUnit
 
 class RegistrationPhoneFragment: Fragment(R.layout.fragment_registration_phone) {
 
+    private lateinit var mPhoneNumber: String
+    private lateinit var mCallback: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onStart() {
         super.onStart()
+
+        mAuth = FirebaseAuth.getInstance()
+
+        mCallback = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                mAuth.signInWithCredential(credential).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        showToast("Welcome")
+                        (activity as RegisterActivity).replaceActivity(MainActivity())
+                    } else{
+                        showToast(it.exception?.message.toString())
+                    }
+                }
+            }
+
+            override fun onVerificationFailed(m: FirebaseException) {
+                showToast(m.message.toString())
+            }
+
+            override fun onCodeSent(id: String, token: PhoneAuthProvider.ForceResendingToken) {
+                replaceFragment(RegistrationCodeFragment(mPhoneNumber, id))
+            }
+        }
 
         register_button_next.setOnClickListener { sendCode() }
     }
@@ -19,7 +54,18 @@ class RegistrationPhoneFragment: Fragment(R.layout.fragment_registration_phone) 
         if (register_input_phone.text.toString().isEmpty()){
             showToast("Enter your phone number")
         } else {
-            replaceFragment(RegistrationCodeFragment())
+            authUser()
         }
+    }
+
+    private fun authUser(){
+        mPhoneNumber = register_input_phone.text.toString()
+        val options = PhoneAuthOptions.newBuilder()
+            .setPhoneNumber(mPhoneNumber)
+            .setTimeout(60, TimeUnit.SECONDS)
+            .setActivity(activity as RegisterActivity)
+            .setCallbacks(mCallback)
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
     }
 }
